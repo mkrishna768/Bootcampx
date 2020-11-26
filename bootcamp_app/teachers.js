d7206a312f743073b18d87e6ba0252ab8e072888
Michael Krishna <michaelkrishna@hotmail.com>
2020-11-26 01:28:56 +0000

    Create teacher query with node postgress

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher, cohorts.name
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach(teacher => {
+    console.log(`${teacher.name}: ${teacher.teacher}`);
+  });
+}).catch(err => console.error('query error', err.stack));
